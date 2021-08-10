9a662a9bc01b0d3cca0d3e4830ac7d5915b29bb4
SAY-droid427 <sayanimallick0218@gmail.com>
2021-08-10 10:01:20 +0530

    Fetching data of search related to keyword

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-const needle=require('needle')
-const config=require('dotenv').config()
-connst 
+const needle = require('needle');
+const token = process.env.BEARER_TOKEN;
+require('dotenv').config()
+
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+async function getRequest() {
+
+   
+    const params = {
+        'query': '#tests'
+    }
+
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
